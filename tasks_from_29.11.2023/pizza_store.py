d6bc26753c29520ae569b2624f7aabc6af8d74e4
DimaPovarich <131833894+DimaPovarich@users.noreply.github.com>
2023-11-29 11:52:32 +0700

    Update pizza_store.py
    update to safe code

--- tasks_from_29.11.2023/pizza_store.py
+++ tasks_from_29.11.2023/pizza_store.py
@@ -9,10 +9,20 @@ def get_to_basket():
             mas.append(j)
             mas.append(i)
             j+=1
-        tovar = int(input(f'какой товар вы хотите положить в корзину?(из этого каталога, обращаться по индексу{mas})'))
+        while True:
+            try:
+                tovar = int(input(f'какой товар вы хотите положить в корзину?(из этого каталога, обращаться по индексу{mas})'))
+                break
+            except ValueError:
+                print('нужно обратиться по индексу!!!')
         basket[mas[tovar*2+1]] = katalog.get(mas[tovar*2+1])
         print(f'{mas[tovar*2+1]} успешно добавлен в корзину')
-        change = int(input('добавить еще товары?(1 - да, 2 - нет): '))
+        while True:
+            try:
+                change = int(input('добавить еще товары?(1 - да, 2 - нет): '))
+                break
+            except ValueError:
+                print('введите цифру!!!')
         if change == 1:
             continue
         else:
@@ -25,17 +35,32 @@ def my_decorator(print_paylist):
         for i in basket:
             sum_basket += basket.get(i)
         while True:
-            change = int(input('вы хотите оплатить онлайн(1 - да, 2 - нет): '))
+            while True:
+                try:
+                    change = int(input('вы хотите оплатить онлайн(1 - да, 2 - нет): '))
+                    break
+                except ValueError:
+                    print('введите цифру!!!')
             if change == 2:
                 print_paylist(basket)
-                cash = int(input('сколько наличных вы даете?'))
+                while True:
+                    try:
+                        cash = int(input('сколько наличных вы даете?'))
+                        break
+                    except ValueError:
+                        print('введите цифру!!!')
                 if cash >= sum_basket:
                     cash_down = cash - sum_basket
                     print(f'корзина успешно оплачена на сумму {sum_basket}, ваша сдача {cash_down}')
                     break
                 else:
                     print('недостаточно наличных средств')
-                    change1 = int(input('оплатить еще раз или выйти(1 - еще раз, 2 - выйти): '))
+                    while True:
+                        try:
+                            change1 = int(input('оплатить еще раз или выйти(1 - еще раз, 2 - выйти): '))
+                            break
+                        except ValueError:
+                            print('введите цифру')
                     if change1 == 1:
                         continue
                     else:
@@ -46,15 +71,14 @@ def my_decorator(print_paylist):
                 break
     return warapper()
 
-
-
-
-
-
 while True:
     print(f'каталог товаров: {katalog}')
-    change = int(input('выберете действие(1 - положить товар в корзину, 2 - оплатить корзину, 3 - показать что у меня в корзине, 4 - выйти из магазина):'))
-
+    while True:
+        try:
+            change = int(input('выберете действие(1 - положить товар в корзину, 2 - оплатить корзину, 3 - показать что у меня в корзине, 4 - выйти из магазина):'))
+            break
+        except ValueError:
+            print('введите цифру!!!')
     if change == 1:
         get_to_basket()
 
@@ -63,18 +87,19 @@ while True:
         def print_paylist(basket):
             cheklist = []
             count_tovar = 1
+            sum = 0
             for i in basket:
                 cheklist.append(list(f'товар №{count_tovar} - {i}, цена: {basket[i]}'))
                 count_tovar += 1
+
             for i in range(len(cheklist)):
                 for j in range(len(cheklist[i])):
                     print(cheklist[i][j], end = ' ')
                 print()
-
-        print('до свидания')
-
     elif change == 3:
         print('ваша корзина', basket)
 
     else:
         break
+print('до свидания')
+
